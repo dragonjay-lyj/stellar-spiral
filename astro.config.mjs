87db63db125f5e41ad99883057c526100a4ca009
dragonjay-lyj <liujinlisb@gmail.com>
2023-09-21 22:37:16 +0800

    new file:   .codesandbox/Dockerfile 	new file:   .gitignore 	new file:   .vercel/output/config.json

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import markdoc from "@astrojs/markdoc";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
+import robotsTxt from "astro-robots-txt";
+import critters from "astro-critters";
+import compressor from "astro-compressor";
+
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://wiki.dragonjay.top',
+  integrations: [tailwind(), markdoc(), mdx(), partytown(), prefetch(), sitemap(), robotsTxt(), astroI18next(), critters(), compressor({
+    gzip: true,
+    brotli: true
+  }), vue()]
+});
